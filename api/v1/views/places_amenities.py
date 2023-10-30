ee0c98c5c569ee6202e56b3183ea2a0c15f5d3de
cindyjeby <cindyjeby@gmail.com>
2023-10-30 03:47:51 -0700

    debug amenity

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """ objects that handle all default RestFul API actions for Place - Amenity """
+
+
 from models.place import Place
 from models.amenity import Amenity
 from models import storage
@@ -9,7 +12,7 @@ from flasgger.utils import swag_from
 from api.v1.views import app_views
 
 
-@app_views.route('places/<place_id>/amenities', methods=['GET'],
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 @swag_from('documentation/place_amenity/get_places_amenities.yml',
            methods=['GET'])
@@ -40,13 +43,9 @@ def delete_place_amenity(place_id, amenity_id):
     Deletes a Amenity object of a Place
     """
     place = storage.get("Place", place_id)
+    amenity = storage,get("Amenity", amenity_id)
 
-    if not place:
-        abort(404)
-
-    amenity = storage.get("Amenity", amenity_id)
-
-    if not amenity:
+    if not plave or not amenity:
         abort(404)
 
     if environ.get('HBNB_TYPE_STORAGE') == "db":
@@ -71,25 +70,19 @@ def post_place_amenity(place_id, amenity_id):
     Link a Amenity object to a Place
     """
     place = storage.get("Place", place_id)
-
-    if not place:
-        abort(404)
-
     amenity = storage.get("Amenity", amenity_id)
 
-    if not amenity:
+    if not place or not amenity:
         abort(404)
 
     if environ.get('HBNB_TYPE_STORAGE') == "db":
         if amenity in place.amenities:
             return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenities.append(amenity)
+        place.amenities.append(amenity)
     else:
         if amenity_id in place.amenity_ids:
             return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenity_ids.append(amenity_id)
+        place.amenity_ids.append(amenity_id)
 
     storage.save()
     return make_response(jsonify(amenity.to_dict()), 201)
